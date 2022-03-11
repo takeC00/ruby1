b084a244124a2cf61052deac415a46b6ad653b0c
服部剛士 <take.614@icloud.com>
2022-03-11 16:28:41 +0900

    include?メソッド実装

--- memo.rb
+++ memo.rb
@@ -10,6 +10,7 @@ if memo_type === "1"
   memo_title = gets.chomp.to_s
   
   puts "ーーーーーーーーーーーーーメモ内容を入力してくださいーーーーーーーーーーーーー"
+  puts "----Ctrl+Dでメモ完了----"
   memo_text = STDIN.read.chomp.to_s
   CSV.open("#{memo_title}.csv",'w') do |csv|
     csv << [memo_text]
@@ -18,22 +19,26 @@ if memo_type === "1"
 elsif memo_type === "2"
   puts "----メモ編集----"
   Dir.glob("**/*.csv") do |item|
-   puts item
+    @item = item
+    puts @item #なぜ26行目の@itemと違うのか
   end
-  puts "ーーーーーーーーーーーーー上記のcsvファイルから、編集したいファイル名を拡張子を除いて入力してくださいーーーーーーーーーーーーー"
+  puts "ーーーーーーーーーーーーー下のcsvファイルから、編集したいファイル名を拡張子を除いて入力してくださいーーーーーーーーーーーーー"
+  puts @item #
   memo_title = gets.chomp.to_s
-  if memo_title = item
-    puts "OK"
-  else
-    puts "NG"
-  end
   
-  CSV.open("#{memo_title}.csv",'a') do |csv|
-    puts "ーーーーーーーーーーーーー#{memo_title}.csvに追記しますーーーーーーーーーーーーー"
-    memo_add = gets.chomp.to_s  
-    csv << [memo_add]
+  
+  if @item.include?(memo_title)
+    puts'OK'
+    
+    CSV.open("#{memo_title}.csv",'a') do |csv|
+      puts "ーーーーーーーーーーーーー#{memo_title}.csvに追記しますーーーーーーーーーーーーー"
+      puts "----Ctrl+Dでメモ完了----"
+      memo_add = STDIN.read.chomp.to_s  
+      csv << [memo_add]
+    end
+  else
+    puts 'NG'
   end
- 
 
 else 
   puts "エラー！！正しい番号を入力してください"
