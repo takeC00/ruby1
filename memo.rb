26f1af071e88b0d74321830f80a2ce522c8b26d0
服部剛士 <take.614@icloud.com>
2022-03-11 12:43:23 +0900

    fix

---
+++ memo.rb
@@ -0,0 +1,37 @@
+require "csv"
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+memo_type = gets.chomp.to_s
+
+#puts memo_type #入力した文字を確認用
+
+if memo_type === "1"
+  puts "----メモ新規作成----"
+  puts "ーーーーーーーーーーーーー拡張子を除いたファイル名を入力して下さいーーーーーーーーーーーーー"
+  memo_title = gets.chomp.to_s
+  
+  puts "ーーーーーーーーーーーーーメモ内容を入力してくださいーーーーーーーーーーーーー"
+  memo_text = gets.chomp.to_s
+  CSV.open("#{memo_title}.csv",'w') do |csv|
+    csv << [memo_text]
+  end
+  
+elsif memo_type === "2"
+  puts "----メモ編集----"
+  Dir.glob("**/csv") do |item|
+   puts item
+  end
+  puts "ーーーーーーーーーーーーー上記のcsvファイルから、編集したいファイル名を拡張子を除いて入力してくださいーーーーーーーーーーーーー"
+  memo_title = gets.chomp.to_s
+  
+  CSV.open("#{memo_title}.csv",'a') do |csv|
+    puts "ーーーーーーーーーーーーー#{memo_title}.csvに追記しますーーーーーーーーーーーーー"
+    memo_add = gets.chomp.to_s  
+    csv << [memo_add]
+  end
+ 
+
+else 
+  puts "エラー！！正しい番号を入力してください"
+end
+
+
